34f0ca9d5f4b55532968f0f8faa10facf19bfef1
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-04 06:35:21 -0300

    Componente BarraSuperior

---
+++ src/components/BarraSuperior/index.tsx
@@ -0,0 +1,21 @@
+import { Container, Busca, InputBusca, ButtonBusca } from "./style";
+import MenuHamburger from "../../assets/icone-menu-hamburger.png";
+import Lupa from "../../assets/icone-lupa.png";
+import Carrinho from "../../assets/icone-carrinho.png";
+
+function BarraSuperior() {
+  return (
+    <Container>
+      <img src={MenuHamburger} alt="Ícone menu hamburger" />
+      <Busca>
+        <InputBusca placeholder="Encontrar na Elektro"></InputBusca>
+        <ButtonBusca type="submit">
+          <img src={Lupa} alt="Botão busca" />
+        </ButtonBusca>
+      </Busca>
+      <img src={Carrinho} alt="Ícone carrinho" />
+    </Container>
+  );
+}
+
+export default BarraSuperior;
