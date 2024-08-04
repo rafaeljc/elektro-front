c80e3bd4dbe74c494beb03a122c5e616c0cc3264
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-04 06:32:53 -0300

    Componente CardProduto

---
+++ src/components/CardProduto/style.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  width: 155px;
+  height: 189px;
+  border: 1px;
+  border-radius: 15.13px;
+  background-color: #F5F5F5;
+  filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.08));
+  padding: 7.51px 9.46px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Imagem = styled.img`
+  height: 127px;
+  border-radius: 15.13px;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Informacoes = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const Nome = styled.h3`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  font-size: 13.24px;
+  color: #2B2B2B;
+`;
+
+export const Preco = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-weight: bold;
+  font-size: 15.13px;
+  color: #2B2B2B;
+`;
