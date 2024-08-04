1493c9b53de0dd1007c676c4da9cb8aabd45e684
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-04 14:22:11 -0300

    Componente InputFormulario

---
+++ src/components/InputFormulario/style.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 24px;
+  margin: 8px 0;
+  border: 0;
+  border-bottom: 2px solid #B9B9B9;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  color: #B9B9B9;
+`;
+
+export const Titulo = styled.h2`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  color: #2B2B2B;
+`;
