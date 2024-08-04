7328c21861fd49f3ec9ef6255d065aa057b1e96d
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-04 06:33:59 -0300

    Componente VitrineProdutos

---
+++ src/components/VitrineProdutos/style.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Vitrine = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+export const Titulo = styled.h2`
+  width: 100%;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  color: #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+export const BarraDivisoria = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: #B9B9B9;
+`;
+
+export const ListaProdutos = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 20px;
+`;
