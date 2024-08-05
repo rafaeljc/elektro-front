fb26ba24313875fed0183bb19c8cd9a2a808c0de
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-05 15:51:12 -0300

    Página Cadastro

---
+++ src/pages/Cadastro/style.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Conteudo = styled.div`
+  width: 100%;
+  padding: 40px 32px 0 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Formulario = styled.form`
+  margin-top: 36px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 16px;
+`;
