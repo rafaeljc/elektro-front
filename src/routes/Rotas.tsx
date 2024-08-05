d57987d29de6debf8b7256ff22be186a5f9e56a8
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-05 09:33:26 -0300

    Adicionada página Perfil às rotas

--- src/routes/Rotas.tsx
+++ src/routes/Rotas.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import PaginaInicial from "../pages/PaginaInicial";
 import Home from "../pages/Home";
+import Perfil from "../pages/Perfil";
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<PaginaInicial />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/perfil" element={<Perfil />} />
       // TODO: rota para página não encontrada (404)
     </Routes>
   );
