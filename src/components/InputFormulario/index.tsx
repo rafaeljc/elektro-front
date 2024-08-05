13f1ebe2a611a20d758c5de03cd2abd0d81a03de
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-05 15:31:13 -0300

    Adicionado opção de type e padding lateral

--- src/components/InputFormulario/index.tsx
+++ src/components/InputFormulario/index.tsx
@@ -3,13 +3,17 @@ import { Container, Input, Titulo } from "./style";
 interface InputFormulario {
   titulo?: string;
   placeholder?: string;
+  type?: string;
+  padding_left?: string;
+  padding_right?: string;
 }
 
 function InputFormulario(props: InputFormulario) {
   return (
     <Container>
       {props.titulo && <Titulo>{props.titulo}</Titulo>}
-      <Input placeholder={props.placeholder} />
+      <Input type={props.type} placeholder={props.placeholder} 
+        padding_left={props.padding_left} padding_right={props.padding_right} />
     </Container>
   );
 }
--- src/components/InputFormulario/style.tsx
+++ src/components/InputFormulario/style.tsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 
+interface Opcoes {
+  padding_left?: string;
+  padding_right?: string;
+}
+
 export const Container = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
 `;
 
-export const Input = styled.input`
+export const Input = styled.input<Opcoes>`
   width: 100%;
   height: 24px;
+  background-color: #F5F5F5;
   margin: 8px 0;
   border: 0;
   border-bottom: 2px solid #B9B9B9;
@@ -17,6 +24,8 @@ export const Input = styled.input`
   font-weight: 400;
   font-size: 16px;
   color: #B9B9B9;
+  padding-left: ${(props: Opcoes) => (props.padding_left ? props.padding_left : "none")};
+  padding-right: ${(props: Opcoes) => (props.padding_right ? props.padding_right : "none")};
 `;
 
 export const Titulo = styled.h2`
